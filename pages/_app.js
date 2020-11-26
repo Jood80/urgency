8e01e468192f2f4b871cceaf61fa4c4ac32d4f9a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-11-26 09:59:02 +0000

    Deploy Jood80/urgency to github.com/Jood80/urgency.git:gh-pages

---
+++ pages/_app.js
@@ -0,0 +1,39 @@
+import React, { useEffect } from 'react'
+import PropTypes from 'prop-types'
+import Head from 'next/head'
+import { ThemeProvider } from '@material-ui/core/styles'
+import { CssBaseline } from '@material-ui/core'
+import theme from '../src/theme'
+
+function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    const jssStyle = document.querySelector('#jss-server-side')
+    if (jssStyle) {
+      jssStyle.parentElement.removeChild(jssStyle)
+    }
+  }, [])
+
+  return (
+    <>
+      <Head>
+        <title>Urgency</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
+}
+
+export default MyApp
+
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+}
---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import { Fragment } from 'react'
+
+export default function Home() {
+  return (
+    <Fragment>
+      <Head>
+        <title>Urgency Site</title>
+      </Head>
+
+      <main>
+        <h1>Landing Page</h1>
+      </main>
+    </Fragment>
+  )
+}
